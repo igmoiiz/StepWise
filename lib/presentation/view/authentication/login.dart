1f93d8df0a4c66bcf208c7d2c55a1cdbcaf0b2a6
Moiz Baloch <khanmoaiz682@gmail.com>
2025-08-25 02:40:36 -0700

    Changed The application icon

--- lib/presentation/view/authentication/login.dart
+++ lib/presentation/view/authentication/login.dart
@@ -11,6 +11,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: []),
     );
   }
