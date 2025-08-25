e78e0102022be8bd817ba4ab65be87b7b502f74a
Moiz Baloch <khanmoaiz682@gmail.com>
2025-08-25 02:30:55 -0700

    Theme Files Implemented! - Moiz Baloch

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:stepwise/firebase_options.dart';
 import 'package:stepwise/presentation/utilities/theme.dart';
 import 'package:stepwise/presentation/view/auth_gate.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
--- lib/presentation/utilities/theme.dart
+++ lib/presentation/utilities/theme.dart
@@ -14,5 +14,13 @@ ThemeData darkMode = ThemeData(
   brightness: Brightness.dark,
   useMaterial3: true,
   fontFamily: GoogleFonts.urbanist().fontFamily,
-  colorScheme: ColorScheme.dark(),
+  colorScheme: ColorScheme.dark(
+    primary: const Color(0xFF1F3A93), // Primary Color -> Main Buttons etc
+    surface: const Color(0xFF121212), //  Background Color
+    onSurface: const Color(0xFF1E1E1E), //  Panels and cards accent Color
+    onPrimary: const Color(0xFFFFFFFF), //  Main Text Color
+    onTertiary: const Color(0xFFBDC3C7), //  Subtext and Labels
+    error: const Color(0xFFE74C3C), //  Error Color
+    secondary: const Color(0xFF27AE60), //  Success Color
+  ),
 );
